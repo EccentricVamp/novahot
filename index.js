a6f3f9caaed2684cf2e2e537609a3eeee8156763
EccentricVamp <EccentricVamp@gmail.com>
2023-03-25 16:11:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+export class Attributes {
+    constructor(
+        body,
+        agility,
+        reaction,
+        strength,
+        charisma,
+        intuition,
+        logic,
+        willpower,
+        initiative
+    ) {
+        this.body = body
+        this.agility = agility
+        this.reaction = reaction
+        this.strength = strength
+        this.charisma = charisma
+        this.intuition = intuition
+        this.logic = logic
+        this.willpower = willpower
+        this.initiative = initiative
+    }
+}
