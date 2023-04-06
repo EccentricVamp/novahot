66ec7c0f4d199094a52d276f39c2c181401b9d5e
EccentricVamp <EccentricVamp@gmail.com>
2023-04-05 21:23:10 -0500

    Update index to use layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import { install } from '@twind/core'
-import { header } from 'components/header.js'
-import { main } from 'components/main.js'
+import { dialog } from 'components/dialog.js'
 import { table } from 'components/table.js'
 import spells from 'data/spells.json' assert { type: 'json' }
-import { config } from './twind.config.js'
-
-install(config)
+import { main } from 'components/main.js'
 
 const categories = [
     'combat',
@@ -17,18 +13,41 @@ const categories = [
 
 const exclude = ['category', 'target']
 
+const attributes = {
+    magic: 5,
+    logic: 5
+}
+
+const skills = {
+    spellcasting: 5
+}
+
 for (const category of categories) {
     const heading = document.createElement('h2')
     heading.classList = 'text-2xl'
-    heading.innerText = category
+    heading.textContent = category
     main.appendChild(heading)
 
     const spellsInCategory = spells
-        .filter(spell => spell.category == category)
+        .filter((spell) => spell.category == category)
 
-    const spellTable = table(spellsInCategory, exclude)
+    const spellTable = table(spellsInCategory, exclude, onClick)
     main.appendChild(spellTable)
 }
 
-document.body.appendChild(header)
-document.body.appendChild(main)
+document.body.appendChild(dialog)
+
+function onClick(event) {
+    const children = event.currentTarget.childNodes;
+    for (const child of children) {
+        if (child.dataset['key'] == 'drain') {
+            console.log(child.textContent)
+            dialog.showModal()
+        }
+    }
+}
+const rows = document.getElementsByTagName('tr')
+for (const row of rows) {
+    row.addEventListener('click', onClick)
+}
+
