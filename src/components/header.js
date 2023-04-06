8e9ac59accfa63b9ff38c3719f51a0663d9e71a1
EccentricVamp <EccentricVamp@gmail.com>
2023-04-05 21:24:09 -0500

    Add roll anchor to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,24 @@
+const URL_ROOT = 'http://127.0.0.1:3000/src'
+const TITLE = 'Novahot'
+
 export const header = document.createElement('header')
-header.classList = 'border-b'
-header.innerHTML = `
-    <h1 class="container mx-auto text-3xl">Novahot</h1>
-`
+header.className = 'border-b'
+
+const container = document.createElement('div')
+container.className = 'container mx-auto flex items-end'
+header.appendChild(container);
+
+const title = document.createElement('h1')
+title.className = 'flex-shrink text-3xl'
+container.appendChild(title)
+
+const titleAnchor = document.createElement('a')
+titleAnchor.href = `${URL_ROOT}/index.html`
+titleAnchor.textContent = TITLE
+title.appendChild(titleAnchor)
+
+const rollAnchor = document.createElement('a')
+rollAnchor.href = `${URL_ROOT}/roll.html`
+rollAnchor.className = 'text-lg mx-3'
+rollAnchor.textContent = 'Roll'
+container.appendChild(rollAnchor)
