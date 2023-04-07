e1b4114b23b7a46222b739bffee05f97fd548d68
EccentricVamp <EccentricVamp@gmail.com>
2023-04-07 10:53:03 -0500

    Add global text and background color

--- src/layout.js
+++ src/layout.js
@@ -4,5 +4,6 @@ import { config } from './twind.config.js'
 import { main } from 'components/main.js'
 
 install(config)
+document.body.className = 'bg-slate-100 text-slate-900'
 document.body.appendChild(header)
 document.body.appendChild(main)
